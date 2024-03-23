b69640fdce300b875c0a4d522042ada06d8111f2
coryo12345 <corydio12@gmail.com>
2024-03-22 20:20:06 -0400

    stub docker functions

--- internal/docker/docker.go
+++ internal/docker/docker.go
@@ -6,3 +6,19 @@ func Health() (bool, error) {
 	// make sure docker can be run
 	return false, errors.New("TODO")
 }
+
+func StartContainer() error {
+	return nil
+}
+
+func BuildImage() error {
+	return nil
+}
+
+func DeleteContainer() error {
+	return nil
+}
+
+func StopContainer() error {
+	return nil
+}
